c6eec5802678f6bd5dfcf9138f2652bb4d462343
student <Palczyk69>
2022-01-13 13:23:02 +0100

    dzialanie przyciskow do przewijania

--- app/src/main/java/com/example/a13012021/MainActivity.kt
+++ app/src/main/java/com/example/a13012021/MainActivity.kt
@@ -1,11 +1,59 @@
 package com.example.a13012021
 
+import android.media.Image
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val nastepne = findViewById<Button>(R.id.nzdj)
+        val poprzednie = findViewById<Button>(R.id.pzdj)
+        var i = 0
+
+        findViewById<ImageView>(R.id.zdjecie).setImageResource(R.drawable.przezr)
+
+        nastepne.setOnClickListener {
+            i++
+            if(i==1){
+                findViewById<ImageView>(R.id.zdjecie).setImageResource(R.drawable.papiez1)
+            }
+            else if(i==2){
+                findViewById<ImageView>(R.id.zdjecie).setImageResource(R.drawable.papiez2)
+            }
+            else if(i==3){
+                findViewById<ImageView>(R.id.zdjecie).setImageResource(R.drawable.papiez3)
+            }
+            else if(i==4){
+                findViewById<ImageView>(R.id.zdjecie).setImageResource(R.drawable.papiexz4)
+            }
+            else if(i==5 || i==0){
+                findViewById<ImageView>(R.id.zdjecie).setImageResource(R.drawable.papiez1)
+                i=1
+            }
+        }
+        poprzednie.setOnClickListener{
+            i--
+            if(i==-1 || i==0){
+                findViewById<ImageView>(R.id.zdjecie).setImageResource(R.drawable.papiexz4)
+                i=4
+            }
+            else if(i==1){
+                findViewById<ImageView>(R.id.zdjecie).setImageResource(R.drawable.papiez1)
+            }
+            else if(i==2){
+                findViewById<ImageView>(R.id.zdjecie).setImageResource(R.drawable.papiez2)
+            }
+            else if(i==3){
+                findViewById<ImageView>(R.id.zdjecie).setImageResource(R.drawable.papiez3)
+            }
+            else if(i==4){
+                findViewById<ImageView>(R.id.zdjecie).setImageResource(R.drawable.papiexz4)
+            }
+        }
     }
 }
